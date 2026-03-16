108e7f13584cce8937460672485aa629905a46c5
martayyilma-oss <martayyilma@gmail.com>
2026-03-16 14:41:00 -0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,10 @@
-
 public class Main {
     public static void main(String[] args) {
-        // --- TODO: Step 1 ---
-        // Create your Student.java file in the src/ folder.
-        // It should have private fields, a constructor, and getters/setters.
-
-        // --- TODO: Step 2 ---
-        // Instantiate your first student object here
-        // Example: Student s1 = new Student("Abebe Kassa", 12345);
-
-        // --- TODO: Step 3 ---
-        // Instantiate your second student object here
         
-        // --- TODO: Step 4 ---
-        // Print the details to the console using your getter methods
-        System.out.println("--- Student Enrollment List ---");
-        // System.out.println("Student Name: " + s1.getName());
+        Student s1 = new Student(name: "Alex", studentId: 1);
+        Student s2 = new Student(name: "Maria", studentId: 2);
 
-        System.out.println("\nAssignment Status: Check your Git commits before pushing!");
+        System.out.printIn(s1.getName());
+        System.out.printIn(s2.getName());
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+
+    private String name;
+    private int studentid;
+
+    public Student(String name, int studentid) {
+        this.name = name;
+        this.studentid = studentid;
+    }
+
+    public String getName() {
+        return name;
+    {
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStudentid() {
+        return studentid;
+    }
+
+    public void setStudentId(int StudentId) {
+        this.studentId = studentId;
+        }
+    }
+
